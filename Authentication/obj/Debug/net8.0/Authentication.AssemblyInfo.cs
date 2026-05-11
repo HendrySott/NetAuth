a9ca26fe382aa3ef4f37c93b66ee49489652fd5f
Hendry <hzee206@gmail.com>
2026-05-11 14:49:49 -0400

    first commitdsddddddeddd

--- Authentication/obj/Debug/net8.0/Authentication.AssemblyInfo.cs
+++ Authentication/obj/Debug/net8.0/Authentication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd845a391f84574ac12ed0454f6cdf3b6bb0b7ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d10662dec39104b8e77dfd729d0f6e5d1c02ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
